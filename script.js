8ad647965f6d85e8267d4e2a2ba582ee0e43e511
Glitch (glitch-hello-website) <none>
2023-10-12 02:58:59 +0000

    👮🙈 Checkpoint ./index.html:61388023/260 ./script.js:61388023/820

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
+// Get references to the search input and search button
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+// Add an event listener to the search button
+searchButton.addEventListener('click', function() {
+    // Get the search query from the input field
+    const query = searchInput.value;
+
+    // Perform the search or redirect to the search results page
+    // Example: window.location.href = '/search-results?query=' + query;
+    console.log('Performing search for query: ' + query);
+});
