957c5bcadd2a50efb8d5345577915f85c5d0f5eb
Glitch (glitch-hello-website) <none>
2023-10-18 20:30:39 +0000

    🎨🦁 Checkpoint ./login.html:61388023/5305 ./index.html:61388023/621 ./login.php:61388023/2239 ./style.css:61388023/841

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+
+// Connect to the database
+$db = new PDO('mysql:host=localhost;dbname=styl', 'root', '');
+
+// Get the username and password from the login form
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Check the username and password against the database
+$sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+$result = $db->query($sql);
+
+// If the username and password are valid, log the user in and redirect them to a protected page
+if ($result->num_rows > 0) {
+  // Set the session cookie
+  $_SESSION['user'] = $username;
+
+  // Redirect the user to a protected page
+  header('Location: protected.php');
+} else {
+  // Display an error message
+  echo 'Invalid username or password.';
+}
+
+?>
