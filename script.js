a39100d3866be78de02f567c099eec4bac845c1b
Glitch (glitch-hello-website) <none>
2023-10-18 20:42:40 +0000

    ☃️🍝 Checkpoint ./style.css:61388023/30 ./index.html:61388023/34 ./script.js:61388023/753

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ searchButton.addEventListener('click', function() {
     // Example: window.location.href = '/search-results?query=' + query;
     console.log('Performing search for query: ' + query);
 });
+
+// Get references to the login button, login modal, and close button
+const loginButton = document.getElementById('loginButton');
+const loginModal = document.getElementById('loginModal');
+const closeButton = document.getElementById('closeButton');
+
+// Open the login form when the login button is clicked
+loginButton.addEventListener('click', function() {
+    loginModal.style.display = 'block';
+});
+
+// Close the login form when the close button is clicked
+closeButton.addEventListener('click', function() {
+    loginModal.style.display = 'none';
+});
+
+// Close the login form when clicking outside the modal
+window.addEventListener('click', function(event) {
+    if (event.target === loginModal) {
+        loginModal.style.display = 'none';
+    }
+});
