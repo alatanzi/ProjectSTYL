226b32d52708b4fbdec87f0489e60920a1204d37
Glitch (glitch-hello-website) <none>
2023-10-18 22:36:48 +0000

    🏅🎒 Checkpoint ./upload.html:61388023/3140 ./script.js:61388023/24696

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-    // Get references to the login button, login modal, and close button
+
+
+// Get references to the login button, login modal, and close button
 const loginButton = document.getElementById('loginButton');
 const loginModal = document.getElementById('loginModal');
 const closeButton = document.getElementById('closeButton');
@@ -17,6 +19,51 @@ closeButton.addEventListener('click', function() {
 window.addEventListener('click', function(event) {
     if (event.target === loginModal) {
         loginModal.style.display = 'none';
+    }
+});
+// This is a simplified example for demonstration purposes. In a real-world application, you should use a secure authentication strategy and store sensitive data securely.
+
+// Sample user data (for demonstration)
+const users = [
+    { username: 'user1', password: 'password1' },
+    { username: 'user2', password: 'password2' },
+];
+
+// Simulate user login and authentication
+function authenticateUser(username, password) {
+    // Search for the user in the users array (this is just a simple demonstration)
+    const user = users.find((user) => user.username === username);
+
+    if (!user) {
+        return { success: false, message: 'User not found' };
+    }
+
+    // Simulate password validation (in a real app, you should hash and compare passwords)
+    if (user.password === password) {
+        return { success: true, user: user, message: 'Login successful' };
+    } else {
+        return { success: false, message: 'Incorrect password' };
+    }
+}
+
+// Event listener for the login form submission
+document.getElementById('loginSubmit').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // Get input values
+    const username = document.querySelector('input[type="text"]').value;
+    const password = document.querySelector('input[type="password"]').value;
+
+    // Authenticate the user
+    const authResult = authenticateUser(username, password);
+
+    if (authResult.success) {
+        // Successful login, you can redirect to a dashboard or perform other actions
+        alert(authResult.message);
+        // Redirect to a secure page or perform other actions here
+    } else {
+        // Failed login, display an error message to the user
+        alert(authResult.message);
     }
 });
 
