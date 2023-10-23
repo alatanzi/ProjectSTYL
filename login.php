87ffe3f250f21416ff4f513bb846d7b433fad319
Glitch (glitch-hello-website) <none>
2023-10-23 22:00:05 +0000

    👢🍵 Checkpoint ./upload.html:61055383/58179 ./login.php:61388023/313

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <?php
 // none of this works currently
 // Connect to the database
-$db = new MongoClient('mongodb+srv://@cluster0.s8h5ohw.mongodb.net/');
+$db = new MongoClient('mongodb+srv://Aaron:P%40ssw0rd@cluster0.s8h5ohw.mongodb.net/');
 
 // Get the username and password from the login form
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 // Check the username and password against the database
-$sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+$sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
 $result = $db->query($sql);
 
 // If the username and password are valid, log the user in and redirect them to a protected page
